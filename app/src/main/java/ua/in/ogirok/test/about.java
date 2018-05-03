69c359ed160b16668fab25e9b961e9e27882ea13
Oleg Luzhnyak <luzhnyak@yandex.ru>
2018-05-03 16:20:03 +0300

    Start Android

---
+++ app/src/main/java/ua/in/ogirok/test/about.java
@@ -0,0 +1,14 @@
+package ua.in.ogirok.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class about extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.about);
+    }
+}
